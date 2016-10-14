2c33855d391b132a00dce48f643f7c4d81722cb0
David Maurer <davemaurermail@gmail.com>
2016-10-13 22:37:54 -0600

    Change before do variable to instance variable so tests can use it.

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -4,11 +4,11 @@ require '../lib/night_writer'
 
 describe NightWriter do
   before do
-    nw = NightWriter.new
+    @nw = NightWriter.new
   end
 
   it 'can take in a file' do
     text = 'The quick brown fox jumped over the lazy dogs.1 fox, 5 dogs, 3 times.10 ways to tuesday.'
-    assert_equal text, nw.process_file('test_text.txt')
+    assert_equal text, @nw.process_file('test_text.txt')
   end
 end
