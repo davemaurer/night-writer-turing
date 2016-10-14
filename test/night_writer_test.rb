f4962cc69f0bf038300ca134dd3b1c23fa6811e5
David Maurer <davemaurermail@gmail.com>
2016-10-14 00:54:27 -0600

    Test read multiline file passing.

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -7,8 +7,15 @@ describe NightWriter do
     @nw = NightWriter.new
   end
 
-  it 'can take in a file' do
+  it 'can take in a one line file' do
     text = 'The quick brown fox jumped over the lazy dogs. 1 fox, 5 dogs, 3 times. 10 ways to tuesday.'
     assert_equal text, @nw.open_file('test_text.txt')
+  end
+
+  it 'can take in a file with multiple lines' do
+    text = 'This is line one.
+This is line two.
+And this is line three.'
+    assert_equal text, @nw.open_file('test_text_multiline.txt')
   end
 end
