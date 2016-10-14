618d0825bcc9a5369d4cab43705e98ad0c6b8433
David Maurer <davemaurermail@gmail.com>
2016-10-13 22:34:04 -0600

    First test. It can read in a file.

---
+++ test/night_writer_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/night_writer'
+
+describe NightWriter do
+  before do
+    nw = NightWriter.new
+  end
+
+  it 'can take in a file' do
+    text = 'The quick brown fox jumped over the lazy dogs.1 fox, 5 dogs, 3 times.10 ways to tuesday.'
+    assert_equal text, nw.process_file('test_text.txt')
+  end
+end
