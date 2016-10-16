28206572f107216af9ac80b218a0fbb8871039ac
David Maurer <davemaurermail@gmail.com>
2016-10-15 20:05:21 -0600

    Break out map_to_keys method for create_braille_line to try incorporating capital letters.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,14 +3,14 @@ class NightWriter
 
   def initialize
     @key = {'a' => '0.....', 'b' => '0.0...', 'c' => '00....', 'd' => '00.0..', 'e' => '0..0..',
-            'f' => '000...', 'g' => '0000..', 'h' => '0.00..', 'i' => '.00...', 'j' => '.000..',
-            'k' => '0...0.', 'l' => '0.0.0.', 'm' => '00..0.', 'n' => '00.00.', 'o' => '0..00.',
-            'p' => '000.0.', 'q' => '00000.', 'r' => '0.000.', 's' => '.00.0.', 't' => '.0000.',
-            'u' => '0...00', 'v' => '0.0.00', 'w' => '.000.0', 'x' => '00..00', 'y' => '00.000',
-            'z' => '0..000', '0' => '.000..', '1' => '0.....', '2' => '0.0...', '3' => '00....',
-            '4' => '00.0..', '5' => '0..0..', '6' => '000...', '7' => '0000..', '8' => '0.00..',
-            '9' => '.00...', '#' => '.0.000', '.' => '..00.0', '?' => '..0.00', '!' => '..000.',
-            "'" => '....0.', ',' => '..0...', '-' => '....00', ' ' => ' ', 'cap' => '.....0'}
+      'f' => '000...', 'g' => '0000..', 'h' => '0.00..', 'i' => '.00...', 'j' => '.000..',
+      'k' => '0...0.', 'l' => '0.0.0.', 'm' => '00..0.', 'n' => '00.00.', 'o' => '0..00.',
+      'p' => '000.0.', 'q' => '00000.', 'r' => '0.000.', 's' => '.00.0.', 't' => '.0000.',
+      'u' => '0...00', 'v' => '0.0.00', 'w' => '.000.0', 'x' => '00..00', 'y' => '00.000',
+      'z' => '0..000', '0' => '.000..', '1' => '0.....', '2' => '0.0...', '3' => '00....',
+      '4' => '00.0..', '5' => '0..0..', '6' => '000...', '7' => '0000..', '8' => '0.00..',
+      '9' => '.00...', '#' => '.0.000', '.' => '..00.0', '?' => '..0.00', '!' => '..000.',
+      "'" => '....0.', ',' => '..0...', '-' => '....00', ' ' => ' ', 'cap' => '.....0'}
   end
 
   def process_lines(file)
@@ -30,11 +30,23 @@ class NightWriter
   end
 
   def create_braille_line(file_string, index_range)
-    message_characters = file_string.chars.map(&:downcase)
-    braille_characters = message_characters.map { |character| @key[character] if @key[character] }
+    message_characters = file_string.chars.map
+    braille_characters = map_to_keys(message_characters)
     line = braille_characters.map { |c| c == ' ' ? ' ' : c[index_range] }
     line.join
   end
+
+  def map_to_keys(characters)
+    characters.map do |character|
+      add_caps(character)
+      (@key[character] if @key[character])
+    end
+  end
+
+  def add_caps(letter)
+    @key['cap'] + @key[letter.downcase] if /[[:upper:]]/.match(letter)
+  end
+
 end
 
 NightWriter.new.write_file(ARGV[0], ARGV[1])
