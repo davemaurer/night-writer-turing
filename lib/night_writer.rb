2940a229afdf02f77e652e2d0599c8a4204561a8
David Maurer <davemaurermail@gmail.com>
2016-10-17 19:38:01 -0600

    Move alphabet key to separate module so I can use key and inverse of key in night_writer and
    night_reader respectively.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,17 +1,7 @@
-class NightWriter
-  attr_reader :key
+require_relative 'translation_keys'
 
-  def initialize
-    @key = {'a' => '0.....', 'b' => '0.0...', 'c' => '00....', 'd' => '00.0..', 'e' => '0..0..',
-            'f' => '000...', 'g' => '0000..', 'h' => '0.00..', 'i' => '.00...', 'j' => '.000..',
-            'k' => '0...0.', 'l' => '0.0.0.', 'm' => '00..0.', 'n' => '00.00.', 'o' => '0..00.',
-            'p' => '000.0.', 'q' => '00000.', 'r' => '0.000.', 's' => '.00.0.', 't' => '.0000.',
-            'u' => '0...00', 'v' => '0.0.00', 'w' => '.000.0', 'x' => '00..00', 'y' => '00.000',
-            'z' => '0..000', '0' => '.000..', '1' => '0.....', '2' => '0.0...', '3' => '00....',
-            '4' => '00.0..', '5' => '0..0..', '6' => '000...', '7' => '0000..', '8' => '0.00..',
-            '9' => '.00...', '#' => '.0.000', '.' => '..00.0', '?' => '..0.00', '!' => '..000.',
-            "'" => '....0.', ',' => '..0...', '-' => '....00', ' ' => ' ', 'cap' => '.....0'}
-  end
+class NightWriter
+  include Keys
 
   def process_lines(file)
     top_lines    = translate_text_to_braille(open_file(file), 0..1).scan(/.{1,160}/)
@@ -53,7 +43,7 @@ class NightWriter
 
   def map_to_keys(characters)
     mapped = characters.map do |character|
-      @key[character] if @key[character]
+      text_to_braille_key[character] if text_to_braille_key[character]
     end
     mapped.compact
   end
